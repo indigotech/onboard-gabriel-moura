aed40c02431fb5e7eb5bbe86451b855c388d8304
gbmoura0606 <gbmoura0606@gmail.com>
2024-01-08 13:05:47 -0300

    Reviewed code: fixing dependencies, removing blank lines

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { ApolloServer } from "apollo-server"
 import { buildSchema } from "graphql"
 
-
 const schema = buildSchema(`
   type Query {
     hello: String
@@ -22,5 +21,5 @@ export const server = new ApolloServer({
 const port = 3000;
 
 server.listen({port}).then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+    console.log(`Server ready at ${url}`);
 });
