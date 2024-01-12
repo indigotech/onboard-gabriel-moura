212743ab7b2db5c9ebf868192cb7f3fae576a7ae
gbmoura0606 <gbmoura0606@gmail.com>
2024-01-12 18:01:47 -0300

    Strong password validation

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,7 @@
 import { User } from './user';
 import { appDataSource } from './setup';
+import { GraphQLError } from 'graphql';
+import { validateStrongPassword } from './input-validation';
 
 interface UserInput {
   name: string;
@@ -16,21 +18,29 @@ export const resolvers = {
   },
   Mutation: {
     createUser: async (_parent: never, args: { data: UserInput }) => {
-      const user = new User();
-      user.name = args.data.name;
-      user.email = args.data.email;
-      user.password = args.data.password;
-      user.birthDate = args.data.birthDate;
+      await appDataSource.initialize();
+      console.log('Conectado ao db local!');
 
-      const userRepository = appDataSource.getRepository(User);
-      await userRepository.save(user);
+      if (validateStrongPassword(args.data.password) && true) {
+        const user = new User();
+        user.name = args.data.name;
+        user.email = args.data.email;
+        user.password = args.data.password;
+        user.birthDate = args.data.birthDate;
 
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        birthDate: user.birthDate,
-      };
+        await appDataSource.manager.save(user);
+        await appDataSource.destroy();
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          birthDate: user.birthDate,
+        };
+      } else {
+        await appDataSource.destroy();
+        throw new GraphQLError('erro: senha fraca!');
+      }
     },
   },
 };
