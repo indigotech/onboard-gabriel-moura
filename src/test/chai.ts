626baa5a774a62c52ae85ba29697d3bac7413a4e
gbmoura0606 <gbmoura0606@gmail.com>
2024-01-18 15:51:24 -0300

    Updating test script, separating tests from index

--- src/test/chai.ts
+++ src/test/chai.ts
@@ -1,12 +1,7 @@
 import axios from 'axios';
-import { launchServer } from './../index';
 import chai from 'chai';
 
 describe('Connect on server and chai expect', () => {
-  before(async () => {
-    await launchServer();
-  });
-
   it('Connection Status Code 200', async () => {
     const response = await axios({
       url: 'http://localhost:3000',
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,28 +1,7 @@
-import axios from 'axios';
 import { launchServer } from './../index';
 
-describe('Testing on server', () => {
-  before(async () => {
-    await launchServer();
-  });
-
-  it('prints Hello World', () => {
-    console.log('Hello World');
-  });
-
-  it('query from server', async () => {
-    const response = await axios({
-      url: 'http://localhost:3000',
-      method: 'post',
-      data: {
-        query: `
-          query Hello {
-            hello
-          }
-        `,
-      },
-    });
-    console.log(response.status);
-    console.log(response.data);
-  });
+before(async () => {
+  await launchServer();
 });
+
+require('./chai');
