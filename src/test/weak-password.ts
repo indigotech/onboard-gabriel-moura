144e85f545252125977ef946c07de5c7e909a779
gbmoura0606 <gbmoura0606@gmail.com>
2024-01-22 10:00:11 -0300

    Improving weak password test

--- src/test/weak-password.ts
+++ src/test/weak-password.ts
@@ -2,40 +2,44 @@ import axios from 'axios';
 import chai from 'chai';
 import { UserInput } from './../resolvers';
 
-describe('Testing returned Custom Error', () => {
-  it('should return invalid password error', async () => {
-    const user: UserInput = {
-      name: 'Taq',
-      email: 'taqnovo1@gmail.com.br',
-      password: 'senhafraca',
-      birthDate: '01-01-2024',
-    };
-
-    const response = await axios({
-      url: 'http://localhost:3000',
-      method: 'post',
-      data: {
-        query: `
-              mutation Mutation($data: UserInput) {
-                createUser(data: $data) {
-                  id
-                  name
-                  email
-                  birthDate
-                }
-              }
-            `,
-        variables: {
-          data: {
-            name: user.name,
-            email: user.email,
-            password: user.password,
-            birthDate: user.birthDate,
-          },
-        },
+const tryWeakPassword = async (user: UserInput) => {
+  const response = await axios({
+    url: 'http://localhost:3000',
+    method: 'post',
+    data: {
+      query: `
+        mutation Mutation ($data: UserInput) {
+          createUser(data: $data) {
+            id
+            name
+            email
+            birthDate
+          }
+        }
+      `,
+      variables: {
+        data: user,
       },
-    });
-    console.log(response.data.errors[0].message);
-    chai.expect(response.data.errors[0].code).to.be.equal(400);
+    },
+  });
+
+  return {
+    code: response.data.errors[0].code,
+    message: response.data.errors[0].message,
+  };
+};
+
+const usersToTest = [
+  { name: 'Taq', email: 'taq@gmail.com', password: 'senhafraca', birthDate: '01-01-2024' },
+  { name: 'Taq', email: 'taq@gmail.com', password: 'F0RT3', birthDate: '01-01-2024' },
+  { name: 'Taq', email: 'taq@gmail.com', password: '12345678', birthDate: '01-01-2024' },
+];
+
+describe('Testing Custom Error weak password message', () => {
+  it('should return weak password error', async () => {
+    for (const user of usersToTest) {
+      chai.expect((await tryWeakPassword(user)).code).to.be.equal(400);
+      chai.expect((await tryWeakPassword(user)).message).to.be.equal('Senha fraca');
+    }
   });
 });
