d7504bac08cd8082acb34a322d30d340a712e939
gbmoura0606 <gbmoura0606@gmail.com>
2024-01-23 10:05:20 -0300

    CR comment: including AfterEach to empty db

--- src/test/create-user.ts
+++ src/test/create-user.ts
@@ -4,20 +4,19 @@ import { UserInput } from '../resolvers';
 import { dataSource } from '../data-source';
 import { User } from '../user';
 
-describe('Testing createUser Mutation', () => {
-  it('should create user', async () => {
-    const user: UserInput = {
-      name: 'Taq',
-      email: 'taq@gmail.com',
-      password: 'senhaforte123',
-      birthDate: '01-01-2024',
-    };
+const usersToTest = [
+  { name: 'Taq', email: 'taq@gmail.com', password: 'senhaforte123', birthDate: '01-01-2024' },
+  { name: 'Taq', email: 'taq@gmail.com', password: 'senhafraca', birthDate: '01-01-2024' },
+  { name: 'Taq', email: 'taq@gmail.com', password: 'F0RT3', birthDate: '01-01-2024' },
+  { name: 'Taq', email: 'taq@gmail.com', password: '12345678', birthDate: '01-01-2024' },
+];
 
-    const response = await axios({
-      url: 'http://localhost:3000',
-      method: 'post',
-      data: {
-        query: `
+const createUser = async (user: UserInput) => {
+  const createUserResponse = await axios({
+    url: 'http://localhost:3000',
+    method: 'post',
+    data: {
+      query: `
             mutation Mutation($data: UserInput) {
               createUser(data: $data) {
                 id
@@ -26,27 +25,34 @@ describe('Testing createUser Mutation', () => {
                 birthDate
               }
             }
-          `,
-        variables: {
-          data: user,
-        },
+        `,
+      variables: {
+        data: user,
       },
+    },
+  });
+  return createUserResponse.data.data.createUser;
+};
+
+afterEach(async () => {
+  await dataSource.getRepository(User).delete({});
+});
+
+describe('Testing createUser Mutation', () => {
+  it('should create user successfully', async () => {
+    const user = usersToTest[0];
+    const createdUser = await createUser(user);
+
+    expect(user.name).to.be.equal(createdUser.name);
+    expect(user.email).to.be.equal(createdUser.email);
+    expect(user.birthDate).to.be.equal(createdUser.birthDate);
+
+    const newUser = await dataSource.getRepository(User).findOneBy({
+      id: createdUser.id,
     });
 
-    expect(user.name).to.be.equal(response.data.data.createUser.name);
-    expect(user.email).to.be.equal(response.data.data.createUser.email);
-    expect(user.birthDate).to.be.equal(response.data.data.createUser.birthDate);
-
-    const createdUser = await dataSource.getRepository(User).findOneBy({
-      id: response.data.data.createUser.id,
-    });
-
-    expect(user.name).to.be.equal(createdUser?.name);
-    expect(user.email).to.be.equal(createdUser?.email);
-    expect(user.birthDate).to.be.equal(createdUser?.birthDate);
-
-    if (createdUser) {
-      await dataSource.getRepository(User).delete(createdUser.id);
-    }
+    expect(user.name).to.be.equal(newUser?.name);
+    expect(user.email).to.be.equal(newUser?.email);
+    expect(user.birthDate).to.be.equal(newUser?.birthDate);
   });
 });
