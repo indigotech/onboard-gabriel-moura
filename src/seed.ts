d7d5cc55b80ce5f3385699c4f7c52885be686f16
gbmoura0606 <gbmoura0606@gmail.com>
2024-02-01 12:26:25 -0300

    end of file line, await db connection

--- src/seed.ts
+++ src/seed.ts
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config({});
 
 export const populateDb = async () => {
-  initializeDbConnection(dataSource);
+  await initializeDbConnection(dataSource);
   const users = [];
 
   for (let i = 0; i < 100; i++) {
